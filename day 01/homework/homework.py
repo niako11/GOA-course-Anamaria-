2edffe8bfdd7edf89608e7b01922d1b488403115
Anamarii129 <anamariakabulashvili32@gmail.com>
2025-03-28 12:23:43 +0400

    commit

---
+++ day 01/homework/homework.py
@@ -0,0 +1,6 @@
+name="Anamaria"
+surname="Kabulashvili"
+age=13
+height=1.60
+love_pet=True
+print(name+" "+surname+" "+str(age)+" years old"+" my height is "+str(height)+ " love pet: "+ str(love_pet))
